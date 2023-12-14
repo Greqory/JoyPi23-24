b355fa2c9f38c4558a4e7595e9767393b9088d92
Gregor <91133008+Greqory@users.noreply.github.com>
2023-12-14 08:04:19 +0100

    Aktualisieren von abschnitt4.py
    refactoring

--- project/abschnitt4.py
+++ project/abschnitt4.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# Import von genutzten Libraries
 import RPi.GPIO as GPIO
 import time
 import dht11
@@ -7,14 +8,13 @@ from adafruit_ht16k33.segments import Seg7x4
 
 i2c = board.I2C() # Initialisierung I2C Bus
 segment = Seg7x4(i2c, address=0x70) #segment der I2C Adresse 0x70 und die Displaydefinition zuweisen
-segment.fill(0) # Initialisierung des Displays. Muss einmal ausgeführt werden bevor das Display benutzt wird.
 
 # Initialisierung GPIO
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.cleanup()
 
-# Pin 4 benutzen, um Daten auslesen 
+# "instance" nutzen, um Daten bei GPIO Pin 4, aus DHT11 auszulesen
 instance = dht11.DHT11(pin = 4)
 
 print ("STRG+C Druecken zum beenden.")
@@ -29,12 +29,11 @@ try:
         segment.fill(0)
 
         # Ausgabe Temperatur aus 7-Segment-Anzeige
-        segment.print(str(int(result.temperature))+'C')
+        segment.print(str(int(result.temperature)) +'C')
         segment.show()
 
         # Verzögerung von 2s
         time.sleep(2) 
-
         # 7-Segment-Anzeige leeren
         segment.fill(0)
 
@@ -45,7 +44,6 @@ try:
         # Verzögerung von 2s
         time.sleep(2) 
 
-# clear display on exit
+# Bei Beenden des Programms wird 7-Segment-Anzeige geleert 
 except KeyboardInterrupt:
     segment.fill(0)
-    pass
